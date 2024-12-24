f5d2bc8aac91913f65f8f0140bb1a5fba49d3044
Hammad Mahmood - Test Automation Engineer <hammad6@outlook.com>
2024-12-24 15:49:18 +0500

    Update NegativeLoginCase.java

--- src/test/java/specs/NegativeLoginCase.java
+++ src/test/java/specs/NegativeLoginCase.java
@@ -1,2 +1,22 @@
-package specs;public class NegativeLoginCase {
+package specs;
+
+import base.AppFactory;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class NegativeLoginCase {
+    @Test
+    public void TestingDriverInitialize() throws InterruptedException {
+
+        String username = "standard_user";
+        String password = "secret_1234";
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.enterUserName(username);
+        loginPage.enterPassword(password);
+
+        loginPage.clickLoginButton();
+
+        Thread.sleep(5000);
+    }
 }
